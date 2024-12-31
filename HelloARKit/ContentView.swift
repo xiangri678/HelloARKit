24facebbcbb9faf46ff830a08bf74eb7145116bf
王冠达 <90504308+xiangri678@users.noreply.github.com>
2024-12-31 11:10:44 +0800

    Initial Commit

---
+++ HelloARKit/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HelloARKit
+//
+//  Created by Chloe Lauren on 12/31/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HelloARKit/HelloARKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloARKitApp.swift
+//  HelloARKit
+//
+//  Created by Chloe Lauren on 12/31/24.
+//
+
+import SwiftUI
+
+@main
+struct HelloARKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
